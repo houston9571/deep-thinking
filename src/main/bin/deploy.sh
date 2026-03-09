cb3985abdf7542c27708df84bfb37a9c93c66721
S90 <houstonn9571@gmail.com>
2026-03-09 17:19:22 +0700

    init de

--- src/main/bin/deploy.sh
+++ src/main/bin/deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-
+APP_NAME=deep-thinking
 JAR_NAME=deep-thinking-1.0.jar
 
 APP_ENV=" --spring.profiles.active=prd --file.encoding=UTF-8"
 JAVA_OPTS=" -server -Xms4g -Xmx4g -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseStringDeduplication -XX:+UseStringDeduplication -XX:+PerfDisableSharedMem"
 
-cd $APP_LOG_DIR ; git checkout -f main; git pull; mvn clean package -U -Dmaven.test.skip=true; cd ..
+cd $APP_NAME ; git checkout -f main; git pull; mvn clean package -U -Dmaven.test.skip=true; cd ..
 
 ps -ef | grep $JAR_NAME | grep -v grep | awk 'NR==1{print \$2}' | xargs -t -r kill -9; sleep 1s;
 
-cp $APP_LOG_DIR/target/$JAR_NAME ./
+cp $APP_NAME/target/$JAR_NAME ./
 
 nohup java $JAVA_OPTS -jar $JAR_NAME $APP_ENV > /dev/null 2>&1 &
 
